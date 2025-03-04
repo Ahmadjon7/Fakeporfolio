f32ee60e7df54a2081b79551bef18395a260ccb0
Ahmadjo7 <karimovahmadjon001@gmail.com>
2025-03-04 06:11:56 +0500

    new fixed bag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Hero from "@/components/Hero/hero"
+import Hero from "@/components/Hero/Hero"
 import './styles/globals.scss'
 import About from "@/components/About/about"
 import Projects from "@/components/Projects/projects"
